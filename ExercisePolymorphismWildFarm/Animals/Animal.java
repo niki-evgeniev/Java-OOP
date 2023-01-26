16d743b36a07ec6f4aa9af7796bb5f6f4dd63622
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-01-27 00:01:37 +0200

    Create Animal.java

---
+++ ExercisePolymorphismWildFarm/Animals/Animal.java
@@ -0,0 +1,55 @@
+package ExercisePolymorphismWildFarm.Animals;
+
+import ExercisePolymorphismWildFarm.Food.Food;
+
+public abstract class Animal {
+    private String animalName;
+    private String animalType;
+    private double animalWeight;
+    private int foodEaten;
+
+    protected Animal(String animalName, String animalType, double animalWeight) {
+        this.animalName = animalName;
+        this.animalType = animalType;
+        this.animalWeight = animalWeight;
+    }
+
+    public String getAnimalName() {
+        return animalName;
+    }
+
+    public void setAnimalName(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public double getAnimalWeight() {
+        return animalWeight;
+    }
+
+    public void setAnimalWeight(double animalWeight) {
+        this.animalWeight = animalWeight;
+    }
+
+    public int getFoodEaten() {
+        return foodEaten;
+    }
+
+    public void setFoodEaten(int foodEaten) {
+        this.foodEaten = foodEaten;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public abstract void makeSound();
+
+
+    public void eat(Food food){
+        this.foodEaten = food.getQuantity();
+    }
+}
