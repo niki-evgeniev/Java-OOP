7a02f22bb3b8c61d2b4c6f0968b48039f1ce2c65
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-01-29 02:55:42 +0200

    Create Mouse.java

---
+++ ExercisePolymorphismWildFarm/Animals/Mouse.java
@@ -0,0 +1,23 @@
+package ExercisePolymorphismWildFarm.Animals;
+
+import ExercisePolymorphismWildFarm.Food.Food;
+import ExercisePolymorphismWildFarm.Food.Meat;
+
+public class Mouse extends Mammal{
+
+    public Mouse(String animalName, String animalType, double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+    @Override
+    public void eat(Food food) {
+        if (food instanceof Meat){
+            throw new IllegalArgumentException("Mice are not eating that type of food!");
+        }
+        super.eat(food);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("SQUEEEAAAK!");
+    }
+}
