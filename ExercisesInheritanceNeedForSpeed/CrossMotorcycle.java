cecb103389f795e113e66914f996df234a35a82f
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-02-04 02:03:15 +0200

    Create CrossMotorcycle.java

---
+++ ExercisesInheritanceNeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package ExercisesInheritanceNeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle{
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
