53be05e8972ce5b88204e045964efab34e44c0aa
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-03 02:36:17 +0200

    Create Motorcycle.java

---
+++ ExercisesInheritanceNeedForSpeed/Motorcycle.java
@@ -0,0 +1,8 @@
+package ExercisesInheritanceNeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
