c40db20093d478d57a3efdc0e5e3f9df6902bcfc
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-07 00:24:44 +0200

    Create ColdBeverage.java

---
+++ ExercisesInheritanceRestaurant/ColdBeverage.java
@@ -0,0 +1,10 @@
+package ExercisesInheritanceRestaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
