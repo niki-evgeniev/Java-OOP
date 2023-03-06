e5f56b208573ea0e71ad0ab98e3eac0a085df171
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-06 02:50:40 +0200

    Create DarkKnight.java

---
+++ ExercisesInheritancePlayersAndMonsters/DarkKnight.java
@@ -0,0 +1,7 @@
+package ExercisesInheritancePlayersAndMonsters;
+
+public class DarkKnight extends Knight{
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
