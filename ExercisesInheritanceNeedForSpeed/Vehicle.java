b6a26bdcd405fe240e7567cf417f92d009f050b6
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-06 02:50:23 +0200

    Create Vehicle.java

---
+++ ExercisesInheritanceNeedForSpeed/Vehicle.java
@@ -0,0 +1,47 @@
+package ExercisesInheritanceNeedForSpeed;
+
+public class Vehicle {
+    final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+//        setFuel(fuel);
+//        setHorsePower(horsePower);
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers){
+        double needFuel = kilometers * fuelConsumption;
+        if (fuel>= needFuel){
+            fuel = fuel - needFuel;
+        }
+    }
+}
