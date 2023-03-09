c7865e63a0a7ae1f1ed48d8a9293a0324cbdc392
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-10 00:48:27 +0200

    Create Rebel.java

---
+++ ExercisesInterfacesАndAbstractionFoodShortage/Rebel.java
@@ -0,0 +1,36 @@
+package ExercisesInterfacesАndAbstractionFoodShortage;
+
+public class Rebel implements Buyer, Person{
+    private String name;
+    private int age;
+    private String group;
+    private int food;
+
+    public Rebel(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public void buyFood() {
+        food = food + 5;
+    }
+
+    @Override
+    public int getFood() {
+        return food;
+    }
+}
