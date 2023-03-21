ad07e84eee6ee72b6520a9d4c95e554207478f0d
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-22 01:18:24 +0200

    Create Command.java

---
+++ reflection/Skeletons/barracksWars/core/commands/Command.java
@@ -0,0 +1,16 @@
+package reflection.Skeletons.barracksWars.core.commands;
+import barracksWars.interfaces.Executable;
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.UnitFactory;
+
+public abstract class Command {
+    private String[] data;
+
+    public Command(String[] data) {
+        this.data = data;
+    }
+
+    public String[] getData() {
+        return data;
+    }
+}
