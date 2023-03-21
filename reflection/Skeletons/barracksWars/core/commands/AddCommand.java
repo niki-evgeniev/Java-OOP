5dd6e5d6dc374707b08e3795eb03e87a756d72a1
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-22 01:18:20 +0200

    Create AddCommand.java

---
+++ reflection/Skeletons/barracksWars/core/commands/AddCommand.java
@@ -0,0 +1,26 @@
+package reflection.Skeletons.barracksWars.core.commands;
+import barracksWars.annotations.Inject;
+import barracksWars.interfaces.Repository;
+import barracksWars.interfaces.Unit;
+import barracksWars.interfaces.UnitFactory;
+import reflection.Skeletons.barracksWars.annotations.Inject;
+
+public class AddCommand extends Command{
+    @Inject
+    private Repository repository;
+    @Inject
+    private UnitFactory unitFactory;
+
+    public AddCommand(String[] data) {
+        super(data);
+    }
+
+    @Override
+    public String execute() {
+        String unitType = getData()[1];
+        Unit unitToAdd = unitFactory.createUnit(unitType);
+        repository.addUnit(unitToAdd);
+        String output = unitType + " added!";
+        return output;
+    }
+}
