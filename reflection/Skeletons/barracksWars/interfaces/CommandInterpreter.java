f853829efca30cc68b070d71c28817a839c74e7c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-22 01:18:44 +0200

    Create CommandInterpreter.java

---
+++ reflection/Skeletons/barracksWars/interfaces/CommandInterpreter.java
@@ -0,0 +1,8 @@
+package reflection.Skeletons.barracksWars.interfaces;
+
+import barracksWars.interfaces.Executable;
+
+public interface CommandInterpreter {
+
+	Executable interpretCommand(String[] data, String commandName);
+}
