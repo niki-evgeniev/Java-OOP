c0c85fd8ba47a0cff679f9b9eef92b4751254dee
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-22 01:18:18 +0200

    Create Inject.java

---
+++ reflection/Skeletons/barracksWars/annotations/Inject.java
@@ -0,0 +1,11 @@
+package reflection.Skeletons.barracksWars.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Inject {
+}
