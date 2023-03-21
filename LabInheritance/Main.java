98770483e78f226c97cc527b4af896818e336cd8
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-22 01:18:10 +0200

    Create Main.java

---
+++ LabInheritance/Main.java
@@ -0,0 +1,16 @@
+package LabInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Dog dog = new Dog();
+        Puppy puppy = new Puppy();
+        puppy.eat();
+        puppy.bark();
+        //puppy.weep();
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+    }
+}
