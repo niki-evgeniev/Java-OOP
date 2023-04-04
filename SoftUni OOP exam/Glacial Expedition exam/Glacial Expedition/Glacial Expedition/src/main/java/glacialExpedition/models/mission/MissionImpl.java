847335deaaf651a8ad1ad4962201f4052e290209
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-04-05 01:07:48 +0300

    exam

---
+++ SoftUni OOP exam/Glacial Expedition exam/Glacial Expedition/Glacial Expedition/src/main/java/glacialExpedition/models/mission/MissionImpl.java
@@ -0,0 +1,22 @@
+package glacialExpedition.models.mission;
+
+import glacialExpedition.models.explorers.Explorer;
+import glacialExpedition.models.states.State;
+
+import java.util.Collection;
+
+public class MissionImpl implements Mission {
+    @Override
+    public void explore(State state, Collection<Explorer> explorers) {
+
+        Collection<String> exhibits = state.getExhibits();
+        for (Explorer explorer : explorers) {
+            while (explorer.canSearch() && exhibits.iterator().hasNext()){
+                explorer.search();
+                String exb = exhibits.iterator().next();
+                explorer.getSuitcase().getExhibits().add(exb);
+                exhibits.remove(exb);
+            }
+        }
+    }
+}
