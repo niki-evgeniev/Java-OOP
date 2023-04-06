9f0001b32972648fa8d01e7b4174d9db96176e72
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-04-07 01:37:45 +0300

    exam

---
+++ SoftUni OOP exam/health/restaurant/src/main/java/restaurant/entities/tables/BaseTable.java
@@ -0,0 +1,100 @@
+package restaurant.entities.tables;
+
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+import restaurant.entities.tables.interfaces.Table;
+
+import java.util.Collection;
+
+import static restaurant.common.ExceptionMessages.INVALID_NUMBER_OF_PEOPLE;
+import static restaurant.common.ExceptionMessages.INVALID_TABLE_SIZE;
+
+public abstract class BaseTable implements Table {
+    private Collection<HealthyFood> healthyFood;
+    private Collection<Beverages> beverages;
+    private int number;
+    private int size;
+    private int numberOfPeople;
+    private double pricePerPerson;
+    private boolean isReservedTable;
+    private double allPeople;
+
+    public BaseTable(int number, int size, double pricePerPerson) {
+        this.number = number;
+        setSize(size);
+        this.pricePerPerson = pricePerPerson;
+    }
+
+    public void setSize(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException(INVALID_TABLE_SIZE);
+        }
+        this.size = size;
+    }
+
+    @Override
+    public int getTableNumber() {
+        return number;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public int numberOfPeople() {
+        return numberOfPeople;
+    }
+
+    @Override
+    public double pricePerPerson() {
+        return pricePerPerson;
+    }
+
+    @Override
+    public boolean isReservedTable() {
+        return isReservedTable;
+    }
+
+    @Override
+    public double allPeople() {
+        return allPeople;
+    }
+
+    @Override
+    public void reserve(int numberOfPeople) {
+        if (numberOfPeople <= 0) {
+            throw new IllegalArgumentException(INVALID_NUMBER_OF_PEOPLE);
+        }
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    @Override
+    public void orderHealthy(HealthyFood food) {
+        healthyFood.add(food);
+    }
+
+    @Override
+    public void orderBeverages(Beverages beverages) {
+        this.beverages.add(beverages);
+    }
+
+    @Override
+    public double bill() {
+        double sum = healthyFood.stream().mapToDouble(HealthyFood::getPrice).sum();
+        sum += beverages.stream().mapToDouble(beverages -> beverages.getPrice()).sum();
+        return sum;
+    }
+
+    @Override
+    public void clear() {
+        healthyFood.clear();
+        beverages.clear();
+    }
+
+    @Override
+    public String tableInformation() {
+        return null;
+    }
+}
