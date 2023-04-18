7a1164c8ea719c6c37c10c945d16a39ad853d7c6
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-04-19 01:29:34 +0300

    exam

---
+++ SoftUni OOP exam/test console/testConsol/src/main/java/org/example/Peson/Person.java
@@ -0,0 +1,13 @@
+package org.example.Peson;
+
+public class Person {
+    private String name;
+    private int age;
+    private double kg;
+
+    public Person(String name, int age, double kg) {
+        this.name = name;
+        this.age = age;
+        this.kg = kg;
+    }
+}
